22e4978cfcafbe5c438f25073c993340b3c1d1b6
Abuton <alaroabubakarolayemi@yahoo.com>
2021-06-06 23:08:19 +0100

    first function 'get_aggs_by_columns' added

--- featureeng/featureeng.py
+++ featureeng/featureeng.py
@@ -1,1 +1,28 @@
 """Main module."""
+import pandas as pd
+
+def get_aggs_by_columns(data:pd.DataFrame, columns:list, agg_list:list, agg_columns:list)->pd.DataFrame:
+    """
+
+    Parameters
+    ----------
+    data:pd.DataFrame : dataframe to calculate FE on
+        
+    columns:list : list of values to aggregate by (the object datatype)
+        
+    agg_list:list : a list of statistical measure e.g mean, median
+        
+    agg_columns:list : a list of numerical datatype columns e.g Age, Salary
+        
+
+    Returns
+    -------
+    A dataframe
+
+    """
+    for cols in columns:
+        for i in agg_list:
+            for j in agg_cols:
+                data[cols+'_'+j+'_'+i] = data.groupby([cols])[j].transform(i)
+
+	return data
--- tests/test_featureeng.py
+++ tests/test_featureeng.py
@@ -11,14 +11,37 @@ from featureeng import featureeng
 @pytest.fixture
 def response():
     """Sample pytest fixture.
-
+    
     See more at: http://doc.pytest.org/en/latest/fixture.html
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+
     """
     # import requests
     # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_get_aggs_by_columns(data:pd.DataFrame, columns:list, agg_list:list, agg_columns:list)->pd.DataFrame:
+    """
+
+    Parameters
+    ----------
+    data:pd.DataFrame :
+        
+    columns:list :
+        
+    agg_list:list :
+        
+    agg_columns:list :
+        
+
+    Returns
+    -------
+
+    """
+
+    return data
